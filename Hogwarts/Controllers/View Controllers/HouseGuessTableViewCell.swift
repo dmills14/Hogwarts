5f9dc6af11d8b9ee9aa02ae6f2c00835ac967ed4
Drew Miller <drew-miller@lambdastudents.com>
2020-07-30 18:47:03 -0700

    HouseGuess Cell

---
+++ Hogwarts/Controllers/View Controllers/HouseGuessTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HouseGuessTableViewCell.swift
+//  Hogwarts
+//
+//  Created by Drew Miller on 7/30/20.
+//  Copyright © 2020 DevMtn. All rights reserved.
+//
+
+import UIKit
+
+class HouseGuessTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
